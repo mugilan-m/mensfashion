b12b6df9803e1554b7ab30200419ee5cad07e1a9
mugilan143 <mugilanm.19cse@kongu.edu>
2022-03-20 19:03:03 +0530

    First commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 const port=process.env.PORT||3000;
 
 const app= express();
