104115b3e2e686c8fa165efd658fd03ddf689398
mugilan143 <mugilanm.19cse@kongu.edu>
2022-03-21 14:47:17 +0530

    new

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ app.use(bodyparser.urlencoded({
     extended:true
 }));
  
+
+//mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true })
+// mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
+const DB ="mongodb+srv://mugilan:19csr111@cluster0.jcb36.mongodb.net/userdb?retryWrites=true&w=majority";
+mongoose.connect(DB 
+ ).then(()=>{
+    console.log("connection successfull");
+}).catch((err)=>console.log(err));
+
+const userSchema =new mongoose.Schema({
+
+    username:String,
+    email:String,
+    password:String
+    
+});
+
+const User = new mongoose.model("user",userSchema);
+
+
+
+
+
+
 app.get("/",function(req,res){
    
     res.render("home");
@@ -27,6 +51,29 @@ app.get("/login",function(req,res){
     res.render("login");
 });
 
+app.get("/index",function(req,res){
+   
+    res.render("index");
+});
+
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:req.body.password
+    });
+    newUser.save(function(err)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else{
+            res.redirect("index");
+        }
+    })
+})
 
 app.listen(port,()=> 
 console.log("server run on port at http://localhost:3000"));
